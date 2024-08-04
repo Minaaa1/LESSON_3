f89e742c9e3a42573b3f8c7eba315fe341d76c40
Minaaa1 <230393@astanait.edu.kz>
2024-08-04 22:11:27 +0500

    Add files via upload

---
+++ mains.js
@@ -0,0 +1,52 @@
+//     #1 Повторить самостоятельно все, что было показано в уроке.
+
+
+//   innerHTML - перезаписывает содержимое элемента 
+//   document  - обращение к нашему документу (JS_Tasks) 
+//   querySelector - обращение к одному из элементов (id, class, p, etc...)
+//   . - обращение к class
+//   # - обращение к id
+
+
+
+// EXAMPLE :
+document.querySelector("#two").innerHTML = "Smm"
+
+// Можно перезаписывать сколько угодно как переменную let :  let a = 5; a = 8;
+
+// document.querySelector("#two").innerHTML = '<h1> Something else </h1>' 
+// document.querySelector("#two").innerHTML = 'jggjgj' 
+// document.querySelector("#two").innerHTML = ' 666 ' 
+//                      /\
+//                      ||
+// В результате : 666 (в заголовке, предыдущие варианты стираются {Something else, jggjgj})
+
+
+
+// не работает (почему ???): 
+/* 
+let btn_1 = querySelector('.btn'); // ReferenceError: querySelector is not defined
+let text_toWrite = querySelector('.write');
+
+btn_1.onclick = function() {
+//console.log('works!');
+console.log(text_toWrite.value);
+
+} 
+ */
+
+
+
+
+
+
+//     #2 С помощью оператора if else сделать проверку: если число больше 100 – вывод на страницу, если число меньше 100 - вывод числа в консоль.
+
+let num = prompt("Write number:");
+let div = document.querySelector(".out")
+if(num > 100) {
+    div.innerHTML = num
+}
+else {
+    console.log(num);
+}
